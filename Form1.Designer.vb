e4dda510697b3f83aa1a7de4d0bb98b19fc28056
Jaden Torres <156242643+Jaden-torres@users.noreply.github.com>
2024-04-01 15:31:08 -0400

    Assignment P3

---
+++ Form1.Designer.vb
@@ -0,0 +1,317 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        PictureBox1 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Cheese = New RadioButton()
+        Pinwheel = New RadioButton()
+        Veggie = New RadioButton()
+        SausagenCheese = New RadioButton()
+        Fruit = New RadioButton()
+        GroupBox1 = New GroupBox()
+        Ignore = New RadioButton()
+        RadioButton1 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        GroupBox2 = New GroupBox()
+        Ignore2 = New RadioButton()
+        Label3 = New Label()
+        Label4 = New Label()
+        Input = New TextBox()
+        Calculate = New Button()
+        Clear = New Button()
+        Answer = New GroupBox()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
+        GroupBox2.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), Image)
+        PictureBox1.Location = New Point(737, 39)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(558, 358)
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Rockwell", 48F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(79, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(436, 106)
+        Label1.TabIndex = 1
+        Label1.Text = "Catering"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Rockwell", 22F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(136, 145)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(253, 49)
+        Label2.TabIndex = 2
+        Label2.Text = "Star Market"
+        ' 
+        ' Cheese
+        ' 
+        Cheese.AutoSize = True
+        Cheese.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Cheese.Location = New Point(34, 62)
+        Cheese.Name = "Cheese"
+        Cheese.Size = New Size(254, 26)
+        Cheese.TabIndex = 3
+        Cheese.TabStop = True
+        Cheese.Text = "Gourmet Cheese $49.99"
+        Cheese.UseVisualStyleBackColor = True
+        ' 
+        ' Pinwheel
+        ' 
+        Pinwheel.AutoSize = True
+        Pinwheel.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Pinwheel.Location = New Point(34, 107)
+        Pinwheel.Name = "Pinwheel"
+        Pinwheel.Size = New Size(249, 26)
+        Pinwheel.TabIndex = 4
+        Pinwheel.TabStop = True
+        Pinwheel.Text = "Pinwheel Wraps $59.99"
+        Pinwheel.UseVisualStyleBackColor = True
+        ' 
+        ' Veggie
+        ' 
+        Veggie.AutoSize = True
+        Veggie.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Veggie.Location = New Point(34, 155)
+        Veggie.Name = "Veggie"
+        Veggie.Size = New Size(169, 26)
+        Veggie.TabIndex = 5
+        Veggie.TabStop = True
+        Veggie.Text = "Veggie $29.99"
+        Veggie.UseVisualStyleBackColor = True
+        ' 
+        ' SausagenCheese
+        ' 
+        SausagenCheese.AutoSize = True
+        SausagenCheese.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        SausagenCheese.Location = New Point(34, 202)
+        SausagenCheese.Name = "SausagenCheese"
+        SausagenCheese.Size = New Size(291, 26)
+        SausagenCheese.TabIndex = 6
+        SausagenCheese.TabStop = True
+        SausagenCheese.Text = "Sausage and Cheese $49.99"
+        SausagenCheese.UseVisualStyleBackColor = True
+        ' 
+        ' Fruit
+        ' 
+        Fruit.AutoSize = True
+        Fruit.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Fruit.Location = New Point(34, 248)
+        Fruit.Name = "Fruit"
+        Fruit.Size = New Size(143, 26)
+        Fruit.TabIndex = 7
+        Fruit.TabStop = True
+        Fruit.Text = "Fruit $29.99"
+        Fruit.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(Ignore)
+        GroupBox1.Controls.Add(Fruit)
+        GroupBox1.Controls.Add(SausagenCheese)
+        GroupBox1.Controls.Add(Veggie)
+        GroupBox1.Controls.Add(Pinwheel)
+        GroupBox1.Controls.Add(Cheese)
+        GroupBox1.Location = New Point(79, 247)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(417, 324)
+        GroupBox1.TabIndex = 8
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' Ignore
+        ' 
+        Ignore.AutoSize = True
+        Ignore.Location = New Point(179, 280)
+        Ignore.Name = "Ignore"
+        Ignore.Size = New Size(89, 29)
+        Ignore.TabIndex = 9
+        Ignore.TabStop = True
+        Ignore.Text = "Ignore"
+        Ignore.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RadioButton1.Location = New Point(84, 70)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(105, 26)
+        RadioButton1.TabIndex = 9
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Pre-Pay"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RadioButton2.Location = New Point(84, 117)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(186, 26)
+        RadioButton2.TabIndex = 10
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Pay upon Pickup"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Controls.Add(Ignore2)
+        GroupBox2.Controls.Add(RadioButton2)
+        GroupBox2.Controls.Add(RadioButton1)
+        GroupBox2.Location = New Point(79, 678)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(394, 170)
+        GroupBox2.TabIndex = 11
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "GroupBox2"
+        ' 
+        ' Ignore2
+        ' 
+        Ignore2.AutoSize = True
+        Ignore2.Location = New Point(215, 67)
+        Ignore2.Name = "Ignore2"
+        Ignore2.Size = New Size(99, 29)
+        Ignore2.TabIndex = 13
+        Ignore2.TabStop = True
+        Ignore2.Text = "Ignore2"
+        Ignore2.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Rockwell", 22F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(79, 932)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(249, 49)
+        Label3.TabIndex = 12
+        Label3.Text = "Please Pay:"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Rockwell", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(737, 547)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(249, 41)
+        Label4.TabIndex = 13
+        Label4.Text = "Loyalty Points"
+        ' 
+        ' Input
+        ' 
+        Input.Location = New Point(992, 547)
+        Input.Name = "Input"
+        Input.Size = New Size(129, 31)
+        Input.TabIndex = 14
+        ' 
+        ' Calculate
+        ' 
+        Calculate.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Calculate.Location = New Point(675, 687)
+        Calculate.Name = "Calculate"
+        Calculate.Size = New Size(217, 65)
+        Calculate.TabIndex = 15
+        Calculate.Text = "Calculate"
+        Calculate.UseVisualStyleBackColor = True
+        ' 
+        ' Clear
+        ' 
+        Clear.Font = New Font("Rockwell", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Clear.Location = New Point(992, 687)
+        Clear.Name = "Clear"
+        Clear.Size = New Size(217, 65)
+        Clear.TabIndex = 16
+        Clear.Text = "Clear"
+        Clear.UseVisualStyleBackColor = True
+        ' 
+        ' Answer
+        ' 
+        Answer.Font = New Font("Rockwell", 14F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Answer.Location = New Point(334, 914)
+        Answer.Name = "Answer"
+        Answer.Size = New Size(961, 91)
+        Answer.TabIndex = 17
+        Answer.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1356, 1041)
+        Controls.Add(Answer)
+        Controls.Add(Clear)
+        Controls.Add(Calculate)
+        Controls.Add(Input)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(PictureBox1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        GroupBox2.ResumeLayout(False)
+        GroupBox2.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Cheese As RadioButton
+    Friend WithEvents Pinwheel As RadioButton
+    Friend WithEvents Veggie As RadioButton
+    Friend WithEvents SausagenCheese As RadioButton
+    Friend WithEvents Fruit As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Ignore As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Ignore2 As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Input As TextBox
+    Friend WithEvents Calculate As Button
+    Friend WithEvents Clear As Button
+    Friend WithEvents Answer As GroupBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,183 @@
+﻿'Jaden Torres'
+'Assignment P3'
+'Sources: Assignment A5'
+
+
+Imports System.Runtime.InteropServices.Marshalling
+Imports Microsoft.VisualBasic.Devices
+
+Public Class Form1
+
+
+    Dim strInput As String 'gather input'
+    Dim intInput As Integer 'gather input into int'
+    Dim CheesePrice As Decimal = 49.99 'price of cheese'
+    Dim PinwheelPrice As Decimal = 59.99 ' price of pinwheel'
+    Dim VeggiePrice As Decimal = 29.99 'price of veggie' 
+    Dim SausagePrice As Decimal = 49.99 'price of sausage'
+    Dim FruitPrice As Decimal = 29.99 'price of fruit'
+    Dim CTool As Decimal 'used to calculate cheese cater'
+    Dim PTool As Decimal 'used to calculate pinwheel cater'
+    Dim VTool As Decimal 'used to calculate veggie cater'
+    Dim STool As Decimal 'used to calculate sausage cater'
+    Dim FTool As Decimal 'used to calculate fruit cater'
+    Dim temp As Decimal 'temp for intInput'
+    Dim Wtemp As Decimal 'temp used for while loop'
+    Dim discount As Decimal 'discount percentage after while loop'
+    Dim subanswer As Decimal 'subanswer calculated from cater calculation'
+    Dim payoption As Decimal 'option chosen from pre pay or in person'
+    Dim item As String 'Item name of the cater type'
+    Dim proc As Decimal 'This tells which button has been checked.'
+    Dim counter As Decimal = 1 'not needed anymore'
+    Dim finalanswer As Decimal 'final answer to be submitted to toString()'
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Loads form'
+        Input.ResetText() 'resets loyalty input'
+        Answer.ResetText() 'resets answer'
+        Ignore.PerformClick() 'clicks ignore button'
+        Ignore.Hide() 'hides ignore button'
+        Ignore2.PerformClick() 'clicks ignore 2 button'
+        Ignore2.Hide() 'hides ignore2 button'
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Calculate_Click(sender As Object, e As EventArgs) Handles Calculate.Click 'what happens when calculate button is pressed'
+        strInput = Input.Text 'sets strInput as the input text'
+        If Integer.TryParse(strInput, intInput) = False Then 'If the input is numeric'
+            MsgBox("error: Please input a whole number")
+            Input.ResetText()
+            Answer.ResetText()
+        Else
+            temp = 0 'temp is reset'
+            temp = intInput 'temp used as another variable to hold the input '
+            If temp < 10 Then 'check if the loyalty points is less that 10, the initial trigger for automatic 5% off'
+                MsgBox("Insignificant loyalty points. Proceeding with checkout.")
+            End If
+            Compute() 'calls compute sub class'
+        End If
+    End Sub
+
+    Private Sub Compute() 'This computes and sends the answer'
+        discount = 0 'resets discount'
+        Wtemp = 0 'resets Wtemp'
+        If temp >= 10 Then 'checks again if the loyalty points is less that 10, the actual trigger for automatic 5% off'
+            discount = 0
+            Wtemp = 0
+            Wtemp = Math.Round(temp / 10) 'divides temp by 10'
+            While Wtemp > 0
+                discount = discount + 0.05 'subtracts Wtemp by one until all rewards points divided by ten is added up'
+                Wtemp = Wtemp - 1
+            End While
+        End If
+
+        If proc = 1 Then 'if option is cheese'
+            subanswer = 0
+            CTool = 0
+            CTool = Math.Round(CheesePrice * discount, 2)
+            subanswer = CheesePrice - CTool
+        Else
+            If proc = 2 Then 'if option is pinwheel'
+                subanswer = 0
+                PTool = 0
+                PTool = Math.Round(PinwheelPrice * discount, 2)
+                subanswer = PinwheelPrice - PTool
+            Else
+                If proc = 3 Then 'if option is veggie'
+                    subanswer = 0
+                    VTool = 0
+                    VTool = Math.Round(VeggiePrice * discount, 2)
+                    subanswer = VeggiePrice - VTool
+                Else
+                    If proc = 4 Then 'if option is sausage'
+                        subanswer = 0
+                        STool = 0
+                        STool = Math.Round(SausagePrice * discount, 2)
+                        subanswer = SausagePrice - STool
+                    Else
+                        If proc = 5 Then 'if option is fruit'
+                            subanswer = 0
+                            FTool = 0
+                            FTool = Math.Round(FruitPrice * discount, 2)
+                            subanswer = FruitPrice - FTool
+                        End If
+                    End If
+                End If
+            End If
+        End If
+        Stat_Output()
+    End Sub
+
+    Private Sub Stat_Output() 'this takes the answer and formats into a output'
+        If payoption = 1 Then 'if Pre-Pay is used'
+            finalanswer = 0
+            finalanswer = subanswer
+            If finalanswer < 0 Then
+                finalanswer = 0
+            End If
+            Answer.Text = "Your order of " + item.ToString() + " costs $" + finalanswer.ToString() + " using Pre-Pay after discount of " + temp.ToString() + " loyalty points." 'total output line'
+        Else
+            If payoption = 2 Then 'if Pay upon pickup is used'
+                finalanswer = 0
+                finalanswer = subanswer
+                If finalanswer < 0 Then
+                    finalanswer = 0
+                End If
+                Answer.Text = "Your order of " + item.ToString() + " costs $" + finalanswer.ToString() + " using Pay upon Pickup after discount of " + temp.ToString() + " loyalty points." 'total output line'
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Clear_Click(sender As Object, e As EventArgs) Handles Clear.Click 'When clear button is pressed'
+        Input.ResetText()
+        Answer.ResetText()
+        Ignore.Show()
+        Ignore2.Show()
+        Ignore.PerformClick()
+        Ignore2.PerformClick()
+        Ignore.Hide()
+        Ignore2.Hide()
+    End Sub
+
+    Private Sub Cheese_CheckedChanged(sender As Object, e As EventArgs) Handles Cheese.CheckedChanged 'checks if cheese button has been pressed'
+        proc = 1
+        item = "Gourmet Cheese"
+    End Sub
+
+    Private Sub Pinwheel_CheckedChanged(sender As Object, e As EventArgs) Handles Pinwheel.CheckedChanged 'checks if pinwheel button has been pressed'
+        proc = 2
+        item = "Pinwheel Wraps"
+    End Sub
+
+    Private Sub Veggie_CheckedChanged(sender As Object, e As EventArgs) Handles Veggie.CheckedChanged 'checks if veggie button has been pressed'
+        proc = 3
+        item = "Veggie"
+    End Sub
+
+    Private Sub SausagenCheese_CheckedChanged(sender As Object, e As EventArgs) Handles SausagenCheese.CheckedChanged 'checks if sausage button has been pressed'
+        proc = 4
+        item = "Sausage and Cheese"
+    End Sub
+
+    Private Sub Fruit_CheckedChanged(sender As Object, e As EventArgs) Handles Fruit.CheckedChanged 'checks if fruit button has been pressed'
+        proc = 5
+        item = "Fruit"
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged 'checks if pre-pay has been pressed'
+        payoption = 1
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged 'checks if pay upon pickup has been pressed'
+        payoption = 2
+    End Sub
+End Class
+
